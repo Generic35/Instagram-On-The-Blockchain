2242eab277f6f69de7f18aa79dbc83f13c0aae25
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-06 14:49:45 -0400

    feat: Add an images mapping to the contract, an upload image function and corresponding test

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,5 +2,22 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
 	string public name = "Decentragram";
-  // Code goes here...
+
+	// Store Images
+	mapping(uint => Image) public images;
+
+	struct Image {
+		uint id;
+		string hash;
+		string description;
+		uint tipAmount;
+		address payable author;
+	}
+
+	// Create Images 
+	function uploadImage() public {
+		images[1] = Image(1, 'abc123', 'Hello world', 0, address(0x0));
+	}
+
+	// Tip Images
 }
