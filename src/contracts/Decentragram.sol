6b47679a246592b32cc4e7b6135ec4847748016c
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-07 09:03:10 -0400

    feat: Add ability to upload image, with emitted event and tests

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -4,6 +4,7 @@ contract Decentragram {
 	string public name = "Decentragram";
 
 	// Store Images
+	uint public imageCount = 0;
 	mapping(uint => Image) public images;
 
 	struct Image {
@@ -14,9 +15,23 @@ contract Decentragram {
 		address payable author;
 	}
 
+	event ImageCreated(
+		uint id, 
+		string hash, 
+		string description, 
+		uint tipAmount, 
+		address payable author
+	);
+
 	// Create Images 
-	function uploadImage() public {
-		images[1] = Image(1, 'abc123', 'Hello world', 0, address(0x0));
+	function uploadImage(string memory _imgHash, string memory _description) public {
+		require(bytes(_description).length > 0);
+		require(bytes(_imgHash).length > 0);
+		require(msg.sender != address(0x0));
+
+		imageCount++; 
+		images[1] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+		emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
 	}
 
 	// Tip Images
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const Decentragram = artifacts.require('./Decentragram.sol');
 
 require('chai')
@@ -27,13 +29,38 @@ contract('Decentragram', ([deployer, author, tipper]) => {
   });
 
   describe('images', async () => {
-    let result;
+    let result, imageCount;
+    const hash = 'abc123';
+
+    before(async () => {
+      result = await decentragram.uploadImage(hash, 'Image description', {
+        from: author,
+      });
+      imageCount = await decentragram.imageCount();
+    });
 
     it('creates images', async () => {
-      result = await decentragram.uploadImage();
-      let image = await decentragram.images(1);
+      // success
+      assert.equal(imageCount, 1);
+      const event = result.logs[0].args;
 
-      console.log(image);
+      assert.equal(event.id.toNumber(), imageCount.toNumber(), 'id is correct');
+      assert.equal(event.hash, hash, 'Hash is correct');
+      assert.equal(
+        event.description,
+        'Image description',
+        'description is correct'
+      );
+      assert.equal(event.tipAmount, '0', 'tip amount is correct');
+      assert.equal(event.author, author, 'author is correct');
+
+      await decentragram.uploadImage('', 'Image description', {
+        from: author,
+      }).should.be.rejected;
+
+      await decentragram.uploadImage('Image hash', '', {
+        from: author,
+      }).should.be.rejected;
     });
   });
 });
