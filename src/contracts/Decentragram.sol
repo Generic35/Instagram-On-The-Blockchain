6eb2e98a9d8b22182c9ce8679704a29568cfa7f8
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-08 09:03:20 -0400

    feat: Add ability to tip the author of an image

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -23,6 +23,14 @@ contract Decentragram {
 		address payable author
 	);
 
+	event ImageTipped(
+		uint id, 
+		string hash, 
+		string description, 
+		uint tipAmount, 
+		address payable author
+	);
+
 	// Create Images 
 	function uploadImage(string memory _imgHash, string memory _description) public {
 		require(bytes(_description).length > 0);
@@ -35,4 +43,24 @@ contract Decentragram {
 	}
 
 	// Tip Images
+	function tipImageOwner(uint _id) public payable {
+		require(_id > 0 && _id <= imageCount);
+		Image memory _image = images[_id];
+		address payable _author = _image.author;
+		address(_author).transfer(msg.value);
+		_image.tipAmount = _image.tipAmount + msg.value;
+		images[_id] = _image;
+
+		emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
+
+
+    // Make sure the id is valid
+    // Fetch the image
+    // Fetch the author
+    // Pay the author by sending them Ether
+    // Increment the tip amount
+    // Update the image
+    // Trigger an event
+
+	}
 }
